4eeca1083ce1a608a1e33152b64422929e526dc7
epilog <epilogen@gmail.com>
2022-02-10 02:33:12 +0100

    Brushing up the css.

--- client/src/QuoteCard/QuoteCard.js
+++ client/src/QuoteCard/QuoteCard.js
@@ -5,7 +5,10 @@ const quotecard = (props) => {
     return (
         <div className="quote-card">
           <div className="quote-text">"{props.text}"</div>
-          <div className="quote-source">- {props.author}, <i>{props.book}</i></div>
+          <div className="quote-source"> 
+            <span className="author">- {props.author},</span> 
+            <span className="book"> {props.book}</span>
+          </div>
         </div>
     );
 };
