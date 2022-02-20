f9f1c22992840f0ef05cfe1565e172c6b4762f72
epilog <epilogen@gmail.com>
2022-02-20 05:15:42 +0100

    Added a string conversion to make html parser happy.

--- client/src/QuoteCard/QuoteCard.js
+++ client/src/QuoteCard/QuoteCard.js
@@ -5,7 +5,7 @@ import parse from 'html-react-parser';
 const quotecard = (props) => {
     return (
         <div className="quote-card">
-          <div className="quote-text">"{parse(props.text)}"</div>
+          <div className="quote-text">"{parse(String(props.text))}"</div>
           <div className="quote-source"> 
             <div className="voice">- {props.voice}</div>
             <div className="source">
